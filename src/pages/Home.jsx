851d5c2a6dc1dc38c06faff4374e5999eca7cff2
TomorchandraRoy <tomorchandraroy884@gmail.com>
2024-12-11 17:22:55 +0600

    review and userreview components added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import Banner from "../components/home/Banner"
+import FeaturedProducts from "../components/home/FeaturedProducts"
+import UserReview from "../components/home/UserReview"
 
 const Home = () => {
   return (
-    <Banner/>
+    <>
+        <Banner/>
+        <div className="my-24">
+            <h1 className="mb-16 text-2xl font-semibold text-center">Featured Products</h1>
+            <FeaturedProducts/>
+        </div>
+        <div className="my-24">
+            <h1 className="mb-16 text-2xl font-semibold text-center">UserReview </h1>
+            <UserReview/>
+        </div>
+    </>
+
   )
 }
 
