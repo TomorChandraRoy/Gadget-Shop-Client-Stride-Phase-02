602e3f3a717000a30dfa87af584824e31c0a58f5
TomorchandraRoy <tomorchandraroy884@gmail.com>
2024-12-14 11:30:19 +0600

    navbar route page create and route create

--- src/utils/routes/Routes.jsx
+++ src/utils/routes/Routes.jsx
@@ -2,6 +2,9 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../../layouts/MainLayout";
 import Home from "../../pages/Home";
+import Aboutus from "../../pages/Aboutus";
+import { Contact } from "../../pages/Contact";
+import Products from "../../pages/Products";
 
 
 export const router = createBrowserRouter([
@@ -10,10 +13,25 @@ export const router = createBrowserRouter([
       element: <MainLayout/>,
       children:[
         {
+          path: "/products",
+          element: <Products />,
+  
+        },
+        {
           path: "/",
           element: <Home />,
   
-        }
+        },
+        {
+          path: "/about",
+          element: <Aboutus />,
+  
+        },
+        {
+          path: "/contect-us",
+          element: <Contact />,
+  
+        },
       ]
     },
   ]);
